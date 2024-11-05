543d6ad5d9cdf539372948742f2d6c33f45ebaf3
RizkyAlde <rizky070603@gmail.com>
2024-11-05 09:46:15 +0700

    update chart

--- src/components/TimeSeries.jsx
+++ src/components/TimeSeries.jsx
@@ -1,81 +1,102 @@
-import {UseTimeSeriesGH} from "@/query/useTimeSeriesGH";
+import { UseTimeSeriesGH } from "@/query/useTimeSeriesGH";
 import dynamic from "next/dynamic";
-import {useState} from "react";
+import { useState } from "react";
 
-const LineChart = dynamic(() => import('@/components/LineChart'), { ssr: false });
+const LineChart = dynamic(() => import("@/components/LineChart"), {
+  ssr: false,
+});
 
-const TimeSeries = ({gh}) => {
-    const [selectedType, setSelectedType] = useState('temp');
+const TimeSeries = ({ gh }) => {
+  const [selectedType, setSelectedType] = useState("temp");
 
-    const {data, isLoading, error} = UseTimeSeriesGH(gh);
+  const { data, isLoading, error } = UseTimeSeriesGH(gh);
 
-    const handleOnChange = (e) => {
-        setSelectedType(e.target.value); // Update the selected type
-    }
+  const handleOnChange = (e) => {
+    setSelectedType(e.target.value); // Update the selected type
+  };
 
-    if (error) return <p className="flex justify-center items-center">Error: {error.message}</p>;
-
-    // Extracting data for the chart
-    const timeLabels = data?.data_sensor.map(sensor => sensor.time).reverse() || [];
-    const humidityData = data?.data_sensor.map(sensor => sensor.humid).reverse() || [];
-    const lumenData = data?.data_sensor.map(sensor => sensor.lumen).reverse() || [];
-    const soilData = data?.data_sensor.map(sensor => sensor.soil).reverse() || [];
-    const temperatureData = data?.data_sensor.map(sensor => sensor.temp).reverse() || [];
-
-    const options = {
-        colors: ['#AED260'],
-        chart: {
-            id: 'basic-bar',
-            type: 'line',
-            height: 350,
-            zoom: {
-                enabled: false,
-            },
-        },
-        xaxis: {
-            categories: timeLabels,
-            title: {
-                text: 'Time',
-            },
-        },
-        title: {
-            text: 'Greenhouse Data Time Series',
-            align: 'center',
-        },
-        yaxis: {
-            title: {
-                text: 'Values',
-            },
-        },
-    };
-
-    const series = [
-        {
-            name: selectedType,
-            data: selectedType === 'temp' ? temperatureData : selectedType === 'soil' ? soilData : selectedType === 'humid' ? humidityData : lumenData,
-        },
-    ];
-
+  if (error)
     return (
-        <div>
-            <div className="mb-4">
-                <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih
-                    Overview</label>
-                <select id="countries" value={selectedType} onChange={handleOnChange}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <option value="temp">Suhu</option>
-                    <option value="soil">Kelembapan Tanah</option>
-                    <option value="humid">Kelembapan Udara</option>
-                    <option value="lumen">Intensitas Cahaya</option>
-                </select>
-            </div>
-            <LineChart
-                options={options}
-                series={series}
-                width="100%"
-            />
-        </div>
-)
-}
+      <p className="flex justify-center items-center">Error: {error.message}</p>
+    );
+
+  // Extracting data for the chart
+  const timeLabels =
+    data?.data_sensor.map((sensor) => sensor.time).reverse() || [];
+  const humidityData =
+    data?.data_sensor.map((sensor) => sensor.humid).reverse() || [];
+  const lumenData =
+    data?.data_sensor.map((sensor) => sensor.lumen).reverse() || [];
+  const soilData =
+    data?.data_sensor.map((sensor) => sensor.soil).reverse() || [];
+  const temperatureData =
+    data?.data_sensor.map((sensor) => sensor.temp).reverse() || [];
+
+  const options = {
+    colors: ["#AED260"],
+    chart: {
+      id: "basic-bar",
+      type: "line",
+      height: 350,
+      zoom: {
+        enabled: false,
+      },
+    },
+    xaxis: {
+      categories: timeLabels,
+      title: {
+        text: "Time",
+      },
+    },
+    title: {
+      text: "Greenhouse Data Time Series",
+      align: "center",
+    },
+    yaxis: {
+      title: {
+        text: "Values",
+      },
+    },
+  };
+
+  const series = [
+    {
+      name: selectedType,
+      data:
+        selectedType === "temp"
+          ? temperatureData
+          : selectedType === "soil"
+          ? soilData
+          : selectedType === "humid"
+          ? humidityData
+          : lumenData,
+    },
+  ];
+
+  return (
+    <div>
+      <div className="mb-4">
+        <label
+          htmlFor="countries"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Pilih Overview
+        </label>
+        <select
+          id="countries"
+          value={selectedType}
+          onChange={handleOnChange}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value="temp">Suhu</option>
+          <option value="soil">Kelembapan Tanah</option>
+          <option value="humid">Kelembapan Udara</option>
+          <option value="lumen">Intensitas Cahaya</option>
+        </select>
+      </div>
+      <LineChart options={options} series={series} width="100%" />
+    </div>
+  );
+};
 
 export default TimeSeries;
--- src/pages/admin/prediction.jsx
+++ src/pages/admin/prediction.jsx
@@ -1,59 +1,103 @@
 import withLayout from "@/layouts/withLayout";
 import DefaultCard from "@/components/DefaultCard";
-import {useState} from "react";
+import { UseProductionPlot } from "@/query/useProductionPlot";
 import dynamic from "next/dynamic";
-import {usePredictPerDay} from "@/query/usePredictPerDay";
-import LineChartPrediction from "@/components/LineChartPrediction";
+import ProductionAllGh from "@/components/ProductionAllGh";
+import LineChartProduction from "@/components/LineChartProduction";
 
-const LineChart = dynamic(() => import('@/components/LineChart'), { ssr: false });
+const ScatterChart = dynamic(() => import("@/components/ScatterChart"), {
+  ssr: false,
+});
+const LineChart = dynamic(() => import("@/components/LineChart"), {
+  ssr: false,
+});
+const BarChart = dynamic(() => import("@/components/BarChart"), { ssr: false });
 
-const Prediction = () => {
+const Production = () => {
+  const { data, isLoading, error } = UseProductionPlot("1");
 
-    const [selectedType, setSelectedType] = useState('1');
-    const {data, isLoading, error} = usePredictPerDay(selectedType);
+  const options = {
+    colors: ["#228B22"],
+    chart: {
+      id: "basic-bar",
+      zoom: {
+        enabled: false,
+      },
+    },
+    xaxis: {
+      title: {
+        text: "Temperature (°C)",
+      },
+      labels: {
+        formatter: (val) => `${val} °C`,
+      },
+    },
+    yaxis: {
+      title: {
+        text: "Humidity (%)",
+      },
+      labels: {
+        formatter: (val) => `${val} %`,
+      },
+    },
+  };
 
-    const handleOnChange = (e) => {
-        setSelectedType(e.target.value); // Update the selected type
-    };
+  const series = [
+    {
+      name: "Temp-Humidity",
+      data: data?.data?.map((point) => [point.x, point.y]) || [], // Ensure it's formatted as [x, y] pairs
+    },
+  ];
 
-    if (isLoading) return <p className="flex justify-center items-center">Loading...</p>;
+  if (isLoading)
+    return <p className="flex justify-center items-center">Loading...</p>;
+  if (error)
+    return (
+      <p className="flex justify-center items-center">Error: {error.message}</p>
+    );
 
   return (
-      <div className="grid w-full grid-cols-1 justify-center">
-          <div className="p-6 w-full bg-white border border-gray-200 rounded-lg shadow mb-4">
-              <h3 className="text-2xl text-green-700 font-semibold text-gray-800 mb-4">Prediksi </h3>
-              <select
-                  value={selectedType}
-                  onChange={handleOnChange}
-                  className="bg-gray-50 border border-gray-300 mb-4 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              >
-                  <option value="1">Greenhouse 1</option>
-                  <option value="2">Greenhouse 2</option>
-                  <option value="3">Greenhouse 3</option>
-                  <option value="4">Greenhouse 4</option>
-                  <option value="5">Greenhouse 5</option>
-                  <option value="6">Greenhouse 6</option>
-                  <option value="7">Greenhouse 7</option>
-                  <option value="8">Greenhouse 8</option>
-                  <option value="9">Greenhouse 9</option>
-                  <option value="10">Greenhouse 10</option>
-                  <option value="11">Greenhouse 11</option>
-                  <option value="12">Greenhouse 12</option>
-              </select>
-              <div className="grid lg:grid-cols-4 gap-4 grid-cols-2">
-                  <DefaultCard image="celcius" subTitle="Celsius (°C)" title="SUHU" value={data && data.Next_Hour_Prediction ? `${data.Next_Hour_Prediction.Predicted_Temperature} %`  : 'N/A' }/>
-                  <DefaultCard image="kelembapan" subTitle="Relative (RH)" title="UDARA" value={data && data.Next_Hour_Prediction ? `${data.Next_Hour_Prediction.Predicted_Humidity} %`  : 'N/A' }/>
-                  <DefaultCard image="cahaya" subTitle="LUX (lux)" title="CAHAYA" value={data && data.Next_Hour_Prediction ? `${data.Next_Hour_Prediction.Predicted_Lumen} %`  : 'N/A' }/>
-                  <DefaultCard image="tanah" subTitle="Relative (RH)" title="TANAH" value={data && data.Next_Hour_Prediction.Predicted_Soil ? `${data.Next_Hour_Prediction.Predicted_Soil} %`  : 'N/A' }/>
-              </div>
+    <div className="grid w-full grid-cols-1 justify-center">
+      <div className="p-3 w-full flex gap-2 flex-col md:flex-row bg-white border border-gray-200 rounded-lg shadow mb-4">
+        <div className="p-6 w-full bg-green-light border border-gray-200 rounded-lg shadow mb-4">
+          <div className="flex justify-between items-center p-2">
+            <h3 className="text-2xl  font-semibold text-gray-800 mb-4">
+              Rata rata Suhu
+            </h3>
+            <h4 className="text-lg p-4 bg-[#336600] rounded-lg font-semibold text-gray-800 mb-4">
+              34 C
+            </h4>
           </div>
-
-          <div className="p-6 w-full bg-white border border-gray-200 rounded-lg shadow mb-4">
-              <h3 className="text-2xl text-green-700 font-semibold text-gray-800 mb-4">Grafik Prediksi</h3>
-              <LineChartPrediction gh={selectedType}/>
+          <ScatterChart width="100%" options={options} series={series} />
+        </div>
+        <div className="p-6 w-full bg-green-light border border-gray-200 rounded-lg shadow mb-4">
+          <div className="flex justify-between gap-2 items-center p-2">
+            <h3 className="text-2xl  font-semibold text-gray-800 mb-4">
+              Intensitas Cahaya
+            </h3>
+            <h4 className="text-lg p-4 bg-[#336600] rounded-lg font-semibold text-gray-800 mb-4">
+              117 lux
+            </h4>
           </div>
+          <ScatterChart width="100%" options={options} series={series} />
+        </div>
       </div>
+      <div className="p-3 w-full flex gap-2 flex-col md:flex-row bg-white border border-gray-200 rounded-lg shadow mb-4">
+        <div className="p-6 w-full bg-green-light border border-gray-200 rounded-lg shadow mb-4">
+          <h3 className="text-2xl  font-semibold text-gray-800 mb-4">
+            Prediksi Jumlah Produksi
+          </h3>
+          <LineChartProduction />
+        </div>
+        <div className="p-6 w-full bg-green-light border border-gray-200 rounded-lg shadow mb-4">
+          <h3 className="text-2xl  font-semibold text-gray-800 mb-4">
+            Produksi Tiap GH
+          </h3>
+          <ProductionAllGh />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
-export default withLayout(Prediction, 'admin');
+export default withLayout(Production, "admin");
